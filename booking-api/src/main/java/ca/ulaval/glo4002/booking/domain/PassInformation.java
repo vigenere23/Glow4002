80ff60083fc1c0aef9d8f6332def2faffc5ed1a4
pamella.cadotte-lawless <pamella.cadotte-lawless@olympus-ossa.com>
2019-09-21 15:17:40 -0400

    Initial object to share pass information.

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/PassInformation.java
@@ -0,0 +1,11 @@
+package ca.ulaval.glo4002.booking.domain;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import ca.ulaval.glo4002.booking.domain.enumeration.PassCategory;
+
+public interface PassInformation {
+	PassCategory getPassCategory();
+	List<LocalDate> getDates();
+}
