c7a43e111556fb73f5981e0fd9f995400a137c2d
Marie-Eve Myrand-Lapierre <marie-eve.myrand-lapierre.1@ulaval.ca>
2019-09-21 22:08:05 -0400

    remove useless stuff

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/interfaces/configuration/CustomJsonProvider.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/interfaces/configuration/CustomJsonProvider.java
@@ -1,5 +1,3 @@
-package ca.ulaval.glo4002.booking.interfaces.configuration;
-
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.Provider;
@@ -12,13 +10,12 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.util.StdDateFormat;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-import ca.ulaval.glo4002.booking.domain.FestivalManager;
-
 /**
- * This class uses the jackson modules to enable proper date/java8 stuff formatting.
- * See : https://github.com/FasterXML/jackson-modules-java8
+ * This class uses the jackson modules to enable proper date/java8 stuff
+ * formatting. See : https://github.com/FasterXML/jackson-modules-java8
  *
- * You can modify it as you wish to suit your needs, but do not set the "indent output" option.
+ * You can modify it as you wish to suit your needs, but do not set the "indent
+ * output" option.
  */
 @Provider
 @Produces(MediaType.APPLICATION_JSON)
@@ -27,16 +24,15 @@ public class CustomJsonProvider extends JacksonJaxbJsonProvider {
     private static ObjectMapper mapper = new ObjectMapper();
 
     static {
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.registerModule(new JavaTimeModule());
-      //  mapper.registerModule(new FestivalManager());
-        mapper.findAndRegisterModules();
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.setDateFormat(new StdDateFormat());
-     }
+	mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	mapper.registerModule(new JavaTimeModule());
+	mapper.findAndRegisterModules();
+	mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+	mapper.setDateFormat(new StdDateFormat());
+    }
 
     public CustomJsonProvider() {
-        super();
-        setMapper(mapper);
+	super();
+	setMapper(mapper);
     }
 }
