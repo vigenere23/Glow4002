0d132eb9cddab97988eb5aebfd112f3efa69d9ab
pamella.cadotte-lawless <pamella.cadotte-lawless@olympus-ossa.com>
2019-09-24 01:10:04 -0400

    Refactoring oxygen services bases.

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/oxygenService/OxygenGradeANeed.java
+++
@@ -1,8 +0,0 @@
-package ca.ulaval.glo4002.booking.domain.oxygenService;
-
-public class OxygenGradeANeed extends OxygenNeed {
-    public OxygenGradeANeed() {
-	oxygenGrade = OxygenGrade.A;
-	oxygenTankQuantity = 3;
-    }
-}
--- booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/pressurizedGaz/OxygenInventoryStoreTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/pressurizedGaz/OxygenInventoryStoreTest.java
@@ -1,4 +1,4 @@
-package ca.ulaval.glo4002.booking.domain.oxygenService;
+package ca.ulaval.glo4002.booking.domain.pressurizedGaz;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +16,7 @@ import ca.ulaval.glo4002.booking.domain.passOrdering.PassCategory;
 
 public class OxygenInventoryStoreTest {
 
-    private OxygenInventoryStore oxygenInventoryStore;
+    private OxygenInventory OxygenInventory;
     final static LocalDate festivalStartingDate = LocalDate.of(2050, 07, 17);
     final static LocalDate oneMonthBeforeFestivalDate = LocalDate.of(2050, 6, 17);
     final static LocalDate fifteenDaysBeforeFestivalDate = LocalDate.of(2050, 7, 2);
@@ -25,7 +25,7 @@ public class OxygenInventoryStoreTest {
 
     @BeforeEach
     public void testInitialize() {
-	oxygenInventoryStore = new OxygenInventoryStore();
+	OxygenInventory = new OxygenInventory();
 	oneDate = new ArrayList<LocalDate>();
 	oneDate.add(LocalDate.of(2050, 7, 18));
     }
@@ -33,30 +33,30 @@ public class OxygenInventoryStoreTest {
     @Test
     public void whenNoPassIsAdded_eachGradeHasQuantityZero() {
 	Arrays.asList(OxygenGrade.values())
-		.forEach(oxygenGrade -> assertEquals(oxygenInventoryStore.getInventory(oxygenGrade), 0));
+		.forEach(oxygenGrade -> assertEquals(OxygenInventory.getInventory(oxygenGrade), 0));
     }
 
     @Test
     public void whenAddSupernovaPass_oxygenGradeEHasQuantityFive() {
 	Orderable supernovaPass = new Pass(oneMonthBeforeFestivalDate, PassCategory.SUPERNOVA, oneDate);
-	oxygenInventoryStore.orderOxygen(supernovaPass);
-	int inventory = oxygenInventoryStore.getInventory(OxygenGrade.E);
+	OxygenInventory.orderOxygen(supernovaPass);
+	int inventory = OxygenInventory.getInventory(OxygenGrade.E);
 	assertEquals(inventory, 5);
     }
 
     @Test
     public void whenAddSupergiantPass_oxygenGradeBHasQuantityThree() {
 	Orderable supergiantPass = new Pass(oneMonthBeforeFestivalDate, PassCategory.SUPERGIANT, oneDate);
-	oxygenInventoryStore.orderOxygen(supergiantPass);
-	int inventory = oxygenInventoryStore.getInventory(OxygenGrade.B);
+	OxygenInventory.orderOxygen(supergiantPass);
+	int inventory = OxygenInventory.getInventory(OxygenGrade.B);
 	assertEquals(inventory, 3);
     }
 
     @Test
     public void whenAddNebulaPass_oxygenGradeAHasQuantityThree() {
 	Orderable nebulaPass = new Pass(oneMonthBeforeFestivalDate, PassCategory.NEBULA, oneDate);
-	oxygenInventoryStore.orderOxygen(nebulaPass);
-	int inventory = oxygenInventoryStore.getInventory(OxygenGrade.A);
+	OxygenInventory.orderOxygen(nebulaPass);
+	int inventory = OxygenInventory.getInventory(OxygenGrade.A);
 	assertEquals(inventory, 3);
     }
 }
