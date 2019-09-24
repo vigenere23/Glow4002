d2f0b6ab7d27376ed62de9e9845e1051095cd31a
pamella.cadotte-lawless <pamella.cadotte-lawless@olympus-ossa.com>
2019-09-24 01:10:04 -0400

    Refactoring oxygen services bases.

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/pressurizedGaz/OxygenTanks.java
@@ -0,0 +1,44 @@
+package ca.ulaval.glo4002.booking.domain.pressurizedGaz;
+
+import java.time.LocalDate;
+
+public class OxygenTanks {
+    private int fabricationQuantity;
+    private int fabricationTimeInDays;
+    private int totalQuantity = 0;
+    private int storageQuantity = 0;
+
+    public OxygenTanks(int fabricationQuantity, int fabricationTimeInDays) {
+	this.fabricationQuantity = fabricationQuantity;
+	this.fabricationTimeInDays = fabricationTimeInDays;
+    }
+
+    public boolean isEnoughTimeForFabrication(LocalDate orderDate, LocalDate limitDeliveryDate) {
+	LocalDate fabricationCompletionDate = orderDate.plusDays(fabricationTimeInDays);
+
+	if (fabricationCompletionDate.isAfter(limitDeliveryDate)) {
+	    return false;
+	}
+	return true;
+    }
+
+    public void adjustInventory(int requirementQuantity) {
+	if (storageQuantity < requirementQuantity) {
+	    totalQuantity += getNumberOfFabricationsNeeded(requirementQuantity) * fabricationQuantity;
+	}
+
+	adjustStorageInventory(requirementQuantity);
+    }
+
+    private int getNumberOfFabricationsNeeded(int requirementQuantity) {
+	return (int) Math.ceil((double) requirementQuantity / fabricationQuantity);
+    }
+
+    private void adjustStorageInventory(int requirementQuantity) {
+	storageQuantity = totalQuantity - requirementQuantity;
+    }
+
+    public int getTotalQuantity() {
+	return totalQuantity;
+    }
+}
