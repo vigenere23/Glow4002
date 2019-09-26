96f970d1901d179fb935d1a0418c4fb1e461ab07
Marie-Eve Myrand-Lapierre <marie-eve.myrand-lapierre.1@ulaval.ca>
2019-09-25 20:45:50 -0400

    Add fabrication quantity and fabrication time to inventory algorithm

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/pressurizedGaz/OxygenFactory.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/pressurizedGaz/OxygenFactory.java
@@ -3,48 +3,80 @@ package ca.ulaval.glo4002.booking.domain.pressurizedGaz;
 import java.util.EnumMap;
 
 public class OxygenFactory implements OxygenReportable {
-	private int defaultGradeEProduction;
-	private int defaultGradeAProduction;
-	private int defaultGradeBProduction;
-	private EnumMap<OxygenGrade, Integer> oxygenInventory;
-	
-	public OxygenFactory() {
-		oxygenInventory = new EnumMap<OxygenGrade, Integer>(OxygenGrade.class);
-		defaultGradeEProduction = 1;
-		defaultGradeAProduction = 1;
-		defaultGradeBProduction = 1;
-	}    
-	
-    public void orderTemplatedOxygenQuantity(OxygenGrade grade) {
-    	if(oxygenInventory.containsKey(grade)) {
-    	    oxygenInventory.put(grade, oxygenInventory.get(grade) + getProductionQuantity(grade));
-    	} else {
-    		 oxygenInventory.put(grade, getProductionQuantity(grade));
-    	}   	
+    private static final int gradeAFabricationQuantity = 5;
+    private static final int gradeBFabricationQuantity = 3;
+    private static final int gradeEFabricationQuantity = 1;
+    private static final int gradeAFabricationTimeInDays = 20;
+    private static final int gradeBFabricationTimeInDays = 10;
+    private static final int gradeEFabricationTimeInDays = 1;
+    private int defaultGradeERequirement;
+    private int defaultGradeARequirement;
+    private int defaultGradeBRequirement;
+    private EnumMap<OxygenGrade, Integer> fabricationQuantities;
+    private EnumMap<OxygenGrade, Integer> fabricationTimesInDays;
+    private EnumMap<OxygenGrade, OxygenTanks> oxygenInventory;
+
+    public OxygenFactory() {
+	defaultGradeERequirement = 1;
+	defaultGradeARequirement = 1;
+	defaultGradeBRequirement = 1;
+	initializeFabricationQuantities();
+	initializeFabricationTimesInDays();
+	initializeOxygenInventory();
     }
-    
+
+    public void orderTemplatedOxygenQuantity(OxygenGrade grade) {
+	if (oxygenInventory.containsKey(grade)) {
+	    oxygenInventory.get(grade).adjustInventory(getRequirementQuantity(grade));
+	} else {
+	    throw new IllegalArgumentException(String.format("Not possible to order oxygen of grade %s.", grade));
+	}
+    }
+
     public void setTemplatedOxygenOrder(EnumMap<OxygenGrade, Integer> template) {
-    	defaultGradeAProduction = template.containsKey(OxygenGrade.A) ? template.get(OxygenGrade.A) : 1;
-    	defaultGradeEProduction = template.containsKey(OxygenGrade.E) ? template.get(OxygenGrade.E) : 1;
-    	defaultGradeBProduction = template.containsKey(OxygenGrade.B) ? template.get(OxygenGrade.B) : 1;
-	}
-    
+	defaultGradeARequirement = template.containsKey(OxygenGrade.A) ? template.get(OxygenGrade.A) : 1;
+	defaultGradeERequirement = template.containsKey(OxygenGrade.E) ? template.get(OxygenGrade.E) : 1;
+	defaultGradeBRequirement = template.containsKey(OxygenGrade.B) ? template.get(OxygenGrade.B) : 1;
+    }
+
     @Override
-	public int getInventory(OxygenGrade oxygenGrade) {
-		return oxygenInventory.get(oxygenGrade);
+    public int getInventory(OxygenGrade grade) {
+	return oxygenInventory.get(grade).getTotalQuantity();
+    }
+
+    private void initializeFabricationQuantities() {
+	fabricationQuantities = new EnumMap<OxygenGrade, Integer>(OxygenGrade.class);
+	fabricationQuantities.put(OxygenGrade.A, gradeAFabricationQuantity);
+	fabricationQuantities.put(OxygenGrade.B, gradeBFabricationQuantity);
+	fabricationQuantities.put(OxygenGrade.E, gradeEFabricationQuantity);
+    }
+
+    private void initializeFabricationTimesInDays() {
+	fabricationTimesInDays = new EnumMap<OxygenGrade, Integer>(OxygenGrade.class);
+	fabricationTimesInDays.put(OxygenGrade.A, gradeAFabricationTimeInDays);
+	fabricationTimesInDays.put(OxygenGrade.B, gradeBFabricationTimeInDays);
+	fabricationTimesInDays.put(OxygenGrade.E, gradeEFabricationTimeInDays);
+    }
+
+    private void initializeOxygenInventory() {
+	oxygenInventory = new EnumMap<OxygenGrade, OxygenTanks>(OxygenGrade.class);
+	for (OxygenGrade grade : OxygenGrade.values()) {
+	    OxygenTanks oxygenTank = new OxygenTanks(fabricationQuantities.get(grade),
+		    fabricationTimesInDays.get(grade));
+	    oxygenInventory.put(grade, oxygenTank);
 	}
-    
-    private int getProductionQuantity(OxygenGrade grade) {
-    	switch (grade) {
-		case A:
-			return defaultGradeAProduction;
-        case B:
-			return defaultGradeBProduction;
-        case E:
-	        return defaultGradeEProduction;
-		default:
-			 throw new IllegalArgumentException(
-					    String.format("No oxygen need impemented for grade %s.", grade));
-		}
+    }
+
+    private int getRequirementQuantity(OxygenGrade grade) {
+	switch (grade) {
+	case A:
+	    return defaultGradeARequirement;
+	case B:
+	    return defaultGradeBRequirement;
+	case E:
+	    return defaultGradeERequirement;
+	default:
+	    throw new IllegalArgumentException(String.format("No oxygen requirement impemented for grade %s.", grade));
+	}
     }
 }
--- booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/pressurizedGaz/OxygenFactoryTests.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/pressurizedGaz/OxygenFactoryTests.java
@@ -1,4 +1,5 @@
 package ca.ulaval.glo4002.booking.domain.pressurizedGaz;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.EnumMap;
@@ -8,52 +9,44 @@ import org.junit.jupiter.api.Test;
 
 public class OxygenFactoryTests {
 
-	private OxygenFactory oxygenFactory;
-	
-	 @BeforeEach
-	 public void testInitialize() {
-		 oxygenFactory = new OxygenFactory();
-	 }
-	 
-	 @Test
-	 public void given_orderOxygen_when_defaultTemplate_then_oneItenIsAdd() {
-		 orderGradeAndAssertInventory(1, OxygenGrade.A);
-	 }
-	 
-	 @Test
-	 public void when_orderOxugenTwice_then_correspopndingQuantityIsAdd() {
-		 oxygenFactory.orderTemplatedOxygenQuantity(OxygenGrade.A);
-		 oxygenFactory.orderTemplatedOxygenQuantity(OxygenGrade.A);
-		 assertTrue(2 == getReportable().getInventory(OxygenGrade.A));
-	 }
-	 
-	 @Test
-	 public void given_setOneTemplate_when_orderOxugen_then_correspopndingQuantityIsAdd() {
-		 EnumMap<OxygenGrade, Integer> expectedQuantity = new EnumMap<OxygenGrade, Integer>(OxygenGrade.class);
-		 expectedQuantity.put(OxygenGrade.A, 10);
-		 oxygenFactory.setTemplatedOxygenOrder(expectedQuantity);
-		 orderGradeAndAssertInventory(10, OxygenGrade.A);
-	 }
-	 
-	 
-	 @Test
-	 public void given_setMultiTemplate_when_orderOxugen_then_correspopndingQuantityIsAdd() {
-		 EnumMap<OxygenGrade, Integer> expectedQuantity = new EnumMap<OxygenGrade, Integer>(OxygenGrade.class);
-		 expectedQuantity.put(OxygenGrade.A, 10);
-		 expectedQuantity.put(OxygenGrade.B, 2);
-		 expectedQuantity.put(OxygenGrade.E, 5);
-		 oxygenFactory.setTemplatedOxygenOrder(expectedQuantity);
-		 orderGradeAndAssertInventory(10, OxygenGrade.A);
-		 orderGradeAndAssertInventory(2, OxygenGrade.B);
-		 orderGradeAndAssertInventory(5, OxygenGrade.E);
-	 }
-	 
-	 private OxygenReportable getReportable() {
-		 return oxygenFactory;
-	 }
-	 
-	 private void orderGradeAndAssertInventory(int inventoryCount, OxygenGrade grade) {
-		 oxygenFactory.orderTemplatedOxygenQuantity(grade);
-		 assertTrue(inventoryCount == getReportable().getInventory(grade));
-	 }
+    private OxygenFactory oxygenFactory;
+
+    @BeforeEach
+    public void testInitialize() {
+	oxygenFactory = new OxygenFactory();
+    }
+
+    @Test
+    public void given_orderOxygenGradeA_when_defaultTemplate_then_fabricationQuantityIsAdded() {
+	orderGradeAndAssertInventory(OxygenGrade.A, 5);
+    }
+
+    @Test
+    public void given_setOneTemplate_when_orderOxygen_then_multipleOfFabricationQuantitiesAreAdded() {
+	EnumMap<OxygenGrade, Integer> expectedQuantity = new EnumMap<OxygenGrade, Integer>(OxygenGrade.class);
+	expectedQuantity.put(OxygenGrade.A, 6);
+	oxygenFactory.setTemplatedOxygenOrder(expectedQuantity);
+	orderGradeAndAssertInventory(OxygenGrade.A, 10);
+    }
+
+    @Test
+    public void given_setMultiTemplate_when_orderOxygen_then_correspondingMultipleOfFabricationQuantityIsAdded() {
+	EnumMap<OxygenGrade, Integer> expectedQuantity = new EnumMap<OxygenGrade, Integer>(OxygenGrade.class);
+	expectedQuantity.put(OxygenGrade.A, 4);
+	expectedQuantity.put(OxygenGrade.B, 2);
+	expectedQuantity.put(OxygenGrade.E, 3);
+	oxygenFactory.setTemplatedOxygenOrder(expectedQuantity);
+	orderGradeAndAssertInventory(OxygenGrade.A, 5);
+	orderGradeAndAssertInventory(OxygenGrade.B, 3);
+	orderGradeAndAssertInventory(OxygenGrade.E, 3);
+    }
+
+    private OxygenReportable getReportable() {
+	return oxygenFactory;
+    }
+
+    private void orderGradeAndAssertInventory(OxygenGrade grade, int inventoryCount) {
+	oxygenFactory.orderTemplatedOxygenQuantity(grade);
+	assertTrue(inventoryCount == getReportable().getInventory(grade));
+    }
 }
