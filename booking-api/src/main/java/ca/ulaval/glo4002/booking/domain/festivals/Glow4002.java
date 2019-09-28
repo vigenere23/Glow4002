3891ee0bc8602842323961a3d2da2f8b07dafa0c
Yoan Chamberland <yoan.chamberland.1@ulaval.ca>
2019-09-28 18:02:12 -0400

    cherryPick pour base API

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/festivals/Glow4002.java
@@ -0,0 +1,10 @@
+package ca.ulaval.glo4002.booking.domain.festivals;
+
+import java.time.OffsetDateTime;
+
+public class Glow4002 extends Festival {
+
+    public Glow4002(OffsetDateTime startDate, OffsetDateTime endDate, OffsetDateTime saleStartDate, OffsetDateTime saleEndDate) {
+        super(startDate, endDate, saleStartDate, saleEndDate);
+    }
+}
