62ebc7de57a546b86d7a1e0e2c08e16452ac70e9
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2019-09-29 16:46:33 -0400

    review

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/passOrdering/orders/Order.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/passOrdering/orders/Order.java
@@ -1,7 +1,7 @@
 package ca.ulaval.glo4002.booking.domain.passOrdering.orders;
 
 public class Order {
-    // TODO implement
+    // TODO implement in #46 and #43
     private Long id;
 
     public Long getId() {
