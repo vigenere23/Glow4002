0a2341b52f49317b446917b4f6bd8f0e46aee239
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2019-09-30 17:48:56 -0400

    refacted Glow class

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/festivals/Glow4002.java
@@ -0,0 +1,26 @@
+package ca.ulaval.glo4002.booking.domain.festivals;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+public class Glow4002 extends Festival {
+
+    public Glow4002() {
+        super(
+            OffsetDateTime.of(LocalDate.of(2050, 7, 17), LocalTime.MIDNIGHT, ZoneOffset.UTC),
+            OffsetDateTime.of(LocalDate.of(2050, 7, 25), LocalTime.MIDNIGHT.minusSeconds(1), ZoneOffset.UTC),
+            OffsetDateTime.of(LocalDate.of(2050, 1, 1), LocalTime.MIDNIGHT, ZoneOffset.UTC),
+            OffsetDateTime.of(LocalDate.of(2050, 7, 17), LocalTime.MIDNIGHT.minusSeconds(1), ZoneOffset.UTC)
+        );
+    }
+
+    public boolean isEventTime(OffsetDateTime eventDate) {
+        return eventDate.isAfter(this.startDate) && eventDate.isBefore(this.endDate);
+    }
+
+    public boolean isSaleTime(OffsetDateTime eventDate) {
+        return eventDate.isAfter(this.saleStartDate) && eventDate.isBefore(this.saleEndDate);
+    }
+}
