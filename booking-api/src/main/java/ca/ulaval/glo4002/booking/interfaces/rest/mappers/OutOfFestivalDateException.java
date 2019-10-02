91422f7fd6c55cad0f3cf0092226b171e7f15895
Yoan Chamberland <yoan.chamberland.1@ulaval.ca>
2019-10-02 19:50:43 -0400

    transport refactor

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/interfaces/rest/mappers/OutOfFestivalDateException.java
@@ -0,0 +1,30 @@
+package ca.ulaval.glo4002.booking.interfaces.rest.mappers;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import javax.ws.rs.core.MediaType;
+
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
+public class OutOfFestivalDateException extends Exception implements ExceptionMapper<OutOfFestivalDateException> {
+
+    private static final long serialVersionUID = 1L;
+    public OutOfFestivalDateExceptionMessage msg = new OutOfFestivalDateExceptionMessage();
+
+    @JsonCreator
+    public OutOfFestivalDateException() {
+        super();
+        msg.error = "INVALID_EVENT_DATE";
+        msg.description = "event date should be between July 17 2050 and July 24 2050";
+    }
+
+    @Override
+    public Response toResponse(OutOfFestivalDateException exception) {
+        return Response.status(400).entity(msg).type(MediaType.APPLICATION_JSON).build();
+    }
+}
