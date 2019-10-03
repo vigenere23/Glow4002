fc68f5da4ca247dbe2c4a0cede676f6919a44da9
Samuel <samuel.morneau-gamache.1@ulaval.ca>
2019-10-02 22:51:18 -0400

    added tests template for GLow4002, work in progress

---
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/festival/Glow4002Test.java
@@ -0,0 +1,71 @@
+package ca.ulaval.glo4002.booking.domain.festival;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+public class Glow4002Test {
+
+    // TODO necessary fields
+
+    @BeforeAll 
+    public void setUp() {
+        // Glow4002 festival = new Glow4002(repo);
+    }
+
+    @Test
+    public void whenReservingPasses_shouldReturnOrder() {
+
+    }
+
+    @Test
+    public void givenInvalidOrderDate_whenReservingPasses_thenShouldThrowAnException() {
+
+    }
+
+    @Test
+    public void givenFiveNebulaPassesOrderedInTime_thenShouldPutTwentyFiveOxygenTanksOfTypeAInOxygenInventory() {
+
+    }
+
+    @Test
+    public void givenFiveNebulaPassesOrderedTooLate_thenShouldPutFifteenOxygenTanksOfTypeEInOxygenInventory() {
+
+    }
+
+    @Test
+    public void givenTwoNebulaTransactionsOfFourandOnePasses_thenSurplusTanksShouldBeUsedForSecondTransaction() {
+
+    }
+
+    @Test
+    public void givenThreeSupergiantPassesOrderedInTime_thenShouldPutNineOxygenTanksOfTypeBInOxygenInventory() {
+
+    }
+
+    @Test
+    public void givenThreeSupergiantPassesOrderedInTime_thenShouldPutNineOxygenTanksOfTypeEInOxygenInventory() {
+
+    }
+
+    @Test
+    public void givenFourSupernovaPassesOrdered_thenShouldPutFourOxygenTanksOfTypeEInOxygenInventory() {
+
+    }
+
+    @Test
+    public void givenOxygenGradeA_whenOxygenRequesterIsCalled_thenShould() {
+
+    }
+
+    @Test
+    public void givenOxygenGradeB_whenOxygenRequesterIsCalled_thenShould() {
+
+    }
+
+    @Test
+    public void givenOxygenGradeE_whenOxygenRequesterIsCalled_thenShould() {
+
+    }
+
+    //TODO festival transport tests
+}
