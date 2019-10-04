e35f191e180be1f0d23344cea86bdaeb9bd9116c
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2019-10-04 14:02:17 -0400

    fixed ordernotfound error message

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/interfaces/rest/exceptions/OrderNotFoundException.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/interfaces/rest/exceptions/OrderNotFoundException.java
@@ -3,6 +3,6 @@ package ca.ulaval.glo4002.booking.interfaces.rest.exceptions;
 public class OrderNotFoundException extends ClientError {
 
     public OrderNotFoundException(long id) {
-        super(404, ClientErrorType.ORDER_NOT_FOUND, String.format("oder with number %d not found", id));
+        super(404, ClientErrorType.ORDER_NOT_FOUND, String.format("order with number %d not found", id));
     }
 }
