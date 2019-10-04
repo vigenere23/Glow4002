da50fb437d379ea6d44f1eb3d5f797aa90bc44ec
Samuel <samuel.morneau-gamache.1@ulaval.ca>
2019-10-04 01:06:32 -0400

    removed empty lines & code duplication for festival dates in pass factory testing

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/passOrdering/passes/factories/PackagePassFactoryTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/passOrdering/passes/factories/PackagePassFactoryTest.java
@@ -2,11 +2,6 @@ package ca.ulaval.glo4002.booking.domain.passOrdering.passes.factories;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-
 import org.junit.jupiter.api.Test;
 
 import ca.ulaval.glo4002.booking.domain.passOrdering.passes.PassCategory;
@@ -17,28 +12,24 @@ import ca.ulaval.glo4002.booking.domain.passOrdering.passes.passTypes.SupernovaP
 
 public class PackagePassFactoryTest {
     
-    OffsetDateTime festivalStart = OffsetDateTime.of(LocalDate.of(2050, 7, 17), LocalTime.MIDNIGHT, ZoneOffset.UTC);
-    OffsetDateTime festivalEnd = OffsetDateTime.of(LocalDate.of(2050, 7, 25), LocalTime.MIDNIGHT.minusSeconds(1), ZoneOffset.UTC);
-    private PackagePassFactory passFactory = new PackagePassFactory(festivalStart, festivalEnd);
+    FestivalDates festivalDates;
+    private PackagePassFactory passFactory = new PackagePassFactory(festivalDates.festivalStart, festivalDates.festivalEnd);
 
     @Test
     public void shouldCreateNebulaPackagePassInstance() {
         PackagePass pass = passFactory.create(PassCategory.NEBULA);
-
         assertTrue(pass.getClass().equals(NebulaPackagePass.class));
     }
 
     @Test
     public void shouldCreateSupergiantPackagePassInstance() {
         PackagePass pass = passFactory.create(PassCategory.SUPERNOVA);
-
         assertTrue(pass.getClass().equals(SupernovaPackagePass.class));
     }
 
     @Test
     public void shouldCreateSupernovaPackagePassInstance() {
         PackagePass pass = passFactory.create(PassCategory.SUPERGIANT);
-
         assertTrue(pass.getClass().equals(SupergiantPackagePass.class));
     }
 }
--- booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/passOrdering/passes/factories/SinglePassFactoryTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/passOrdering/passes/factories/SinglePassFactoryTest.java
@@ -19,14 +19,12 @@ import ca.ulaval.glo4002.booking.domain.passOrdering.passes.passTypes.SupernovaS
 
 public class SinglePassFactoryTest {
     
-    OffsetDateTime festivalStart = OffsetDateTime.of(LocalDate.of(2050, 7, 17), LocalTime.MIDNIGHT, ZoneOffset.UTC);
-    OffsetDateTime festivalEnd = OffsetDateTime.of(LocalDate.of(2050, 7, 25), LocalTime.MIDNIGHT.minusSeconds(1), ZoneOffset.UTC);
-    private SinglePassFactory passFactory = new SinglePassFactory(festivalStart, festivalEnd);
+    FestivalDates festivalDates;
+    private SinglePassFactory passFactory = new SinglePassFactory(festivalDates.festivalStart, festivalDates.festivalEnd);
     private OffsetDateTime validDate = OffsetDateTime.of(LocalDate.of(2050, 7, 18), LocalTime.MIDNIGHT, ZoneOffset.UTC);
 
     @Test
     public void whenPassingInvalidEventDate_thenAnExceptionIsThrown() {
-        
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
             passFactory.create(PassCategory.NEBULA, OffsetDateTime.now());
         });
@@ -35,21 +33,18 @@ public class SinglePassFactoryTest {
     @Test
     public void shouldCreateNebulaSinglePassInstance() {
         SinglePass pass = passFactory.create(PassCategory.NEBULA, validDate);
-
         assertTrue(pass.getClass().equals(NebulaSinglePass.class));
     }
 
     @Test
     public void shouldCreateSupergiantSinglePassInstance() {
         SinglePass pass = passFactory.create(PassCategory.SUPERNOVA, validDate);
-
         assertTrue(pass.getClass().equals(SupernovaSinglePass.class));
     }
 
     @Test
     public void shouldCreateSupernovaSinglePassInstance() {
         SinglePass pass = passFactory.create(PassCategory.SUPERGIANT, validDate);
-
         assertTrue(pass.getClass().equals(SupergiantSinglePass.class));
     }
 }
