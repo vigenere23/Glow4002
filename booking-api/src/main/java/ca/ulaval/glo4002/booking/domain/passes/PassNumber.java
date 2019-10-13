5b1b360dcd70465ed9eda7ced7eaa08be4154597
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2019-10-13 10:30:44 -0400

    created passNumber

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/passes/PassNumber.java
@@ -0,0 +1,25 @@
+package ca.ulaval.glo4002.booking.domain.passes;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class PassNumber {
+
+    private long value;
+    private static final AtomicLong incrementor = new AtomicLong(0); 
+
+    public PassNumber() {
+        value = incrementor.getAndIncrement();
+    }
+
+    private PassNumber(long value) {
+        this.value = value;
+    }
+
+    public static PassNumber of(long value) {
+        return new PassNumber(value);
+    }
+
+    public Long getValue() {
+        return value;
+    }
+}
