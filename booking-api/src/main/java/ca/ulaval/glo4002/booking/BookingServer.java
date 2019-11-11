8be18e61cb8e6e3804f4f5220caa6de4a19ed320
Yoan Chamberland <yoan.chamberland.1@ulaval.ca>
2019-11-11 13:08:48 -0500

    implement PassRepo

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/BookingServer.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/BookingServer.java
@@ -25,6 +25,7 @@ import ca.ulaval.glo4002.booking.domain.oxygen.OxygenInventoryRepository;
 import ca.ulaval.glo4002.booking.domain.oxygen.OxygenReserver;
 import ca.ulaval.glo4002.booking.domain.passes.PassFactory;
 import ca.ulaval.glo4002.booking.domain.passes.PassPriceFactory;
+import ca.ulaval.glo4002.booking.domain.passes.PassRepository;
 import ca.ulaval.glo4002.booking.domain.program.ProgramValidator;
 import ca.ulaval.glo4002.booking.domain.transport.ShuttleRepository;
 import ca.ulaval.glo4002.booking.domain.transport.TransportReserver;
@@ -34,6 +35,7 @@ import ca.ulaval.glo4002.booking.infrastructure.apiArtistsRepository.dto.ArtistR
 import ca.ulaval.glo4002.booking.infrastructure.persistance.heap.HeapOxygenHistoryRepository;
 import ca.ulaval.glo4002.booking.infrastructure.persistance.heap.HeapOxygenInventoryRepository;
 import ca.ulaval.glo4002.booking.infrastructure.persistance.heap.HeapPassOrderRepository;
+import ca.ulaval.glo4002.booking.infrastructure.persistance.heap.HeapPassRepository;
 import ca.ulaval.glo4002.booking.infrastructure.persistance.heap.HeapShuttleRepository;
 
 
@@ -86,10 +88,11 @@ public class BookingServer implements Runnable {
         TransportUseCase transportUseCase = new TransportUseCase(festivalDates, shuttleRepository);
 
         PassOrderRepository passOrderRepository = new HeapPassOrderRepository();
+        PassRepository passRepository = new HeapPassRepository();
         PassPriceFactory passPriceFactory = new PassPriceFactory();
         PassFactory passFactory = new PassFactory(festivalDates, passPriceFactory);
         PassOrderFactory passOrderFactory = new PassOrderFactory(festivalDates, passFactory);
-        PassOrderUseCase passOrderUseCase = new PassOrderUseCase(passOrderFactory, passOrderRepository, transportReserver, oxygenReserver);
+        PassOrderUseCase passOrderUseCase = new PassOrderUseCase(passOrderFactory, passOrderRepository, transportReserver, oxygenReserver, passRepository);
         ArtistRankingInformationMapper artistRankingInformationMapper = new ArtistRankingInformationMapper();
         externalApiArtist = new ExternalApiArtist();
         ArtistRepository artistsRepository = new ExternalArtistRepository(artistRankingInformationMapper, externalApiArtist);
---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/passes/PassCounter.java
@@ -0,0 +1,9 @@
+package ca.ulaval.glo4002.booking.domain.passes;
+
+import java.util.List;
+
+public class PassCounter {
+    public int countFestivalAttendeesForOneDay(List<Pass> passes) {
+        return 0;
+    }
+}
