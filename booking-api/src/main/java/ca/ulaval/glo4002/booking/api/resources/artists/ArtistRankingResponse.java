73cef75c0e2e1bb761a5a3a70cf4d0ce2f67729f
pamella.cadotte-lawless <pamella.cadotte-lawless@olympus-ossa.com>
2019-11-13 00:19:25 -0500

    Issue#166 Ranking functionnal test

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/api/resources/artists/ArtistRankingResponse.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/api/resources/artists/ArtistRankingResponse.java
@@ -1,13 +1,7 @@
 package ca.ulaval.glo4002.booking.api.resources.artists;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class ArtistRankingResponse {
-    
-    public List<String> artists = new ArrayList<>();
-
-	public ArtistRankingResponse(List<String> artists) {
-        this.artists = artists;
-    }
+public class ArtistRankingResponse {  
+    public List<String> artists;
 }
