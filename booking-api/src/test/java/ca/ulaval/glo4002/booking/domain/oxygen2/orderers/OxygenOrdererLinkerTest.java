7599ebf477d8d0dc7629d5c7aba898702a39d89a
vigenere23 <lolgab1@hotmail.com>
2019-11-18 16:32:52 -0500

    added oxygenOrdererLinkerTest.java

---
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/oxygen2/orderers/OxygenOrdererLinkerTest.java
@@ -0,0 +1,43 @@
+package ca.ulaval.glo4002.booking.domain.oxygen2.orderers;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OxygenOrdererLinkerTest {
+
+    private OxygenOrdererLinker oxygenOrdererLinker;
+    private OxygenOrderer oxygenOrderer1;
+    private OxygenOrderer oxygenOrderer2;
+    private OxygenOrderer oxygenOrderer3;
+    private List<OxygenOrderer> oxygenOrderers;
+
+    @BeforeEach
+    public void setup() {
+        setupOxygenOrderersList();
+        oxygenOrdererLinker = new OxygenOrdererLinker();
+    }
+
+    @Test
+    public void givenListOfOxygenOrderer_whenLinking_itSetsNextEachOrdererWithTheNextOne() {
+        oxygenOrdererLinker.link(oxygenOrderers);
+        verify(oxygenOrderer1).setNextOrderer(oxygenOrderer2);
+        verify(oxygenOrderer2).setNextOrderer(oxygenOrderer3);
+    }
+
+    private void setupOxygenOrderersList() {
+        oxygenOrderer1 = mock(OxygenOrderer.class);
+        oxygenOrderer2 = mock(OxygenOrderer.class);
+        oxygenOrderer3 = mock(OxygenOrderer.class);
+        oxygenOrderers = new ArrayList<OxygenOrderer>() {{
+            add(oxygenOrderer1);
+            add(oxygenOrderer2);
+            add(oxygenOrderer3);
+        }};
+    }
+}
