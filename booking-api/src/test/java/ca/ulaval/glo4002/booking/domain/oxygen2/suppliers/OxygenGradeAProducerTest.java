b019b8ed4fab0d45d2ac1ba472b1370c15d5c77a
vigenere23 <lolgab1@hotmail.com>
2019-11-18 14:48:17 -0500

    addedOxygenGradeATest

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/oxygen2/suppliers/OxygenGradeAProducerTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/oxygen2/suppliers/OxygenGradeAProducerTest.java
@@ -1,15 +1,28 @@
 package ca.ulaval.glo4002.booking.domain.oxygen2.suppliers;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import ca.ulaval.glo4002.booking.domain.Price;
 import ca.ulaval.glo4002.booking.domain.finance.ProfitCalculator;
+import ca.ulaval.glo4002.booking.domain.oxygen2.OxygenCalculator;
 import ca.ulaval.glo4002.booking.domain.oxygen2.OxygenInventory;
 import ca.ulaval.glo4002.booking.domain.oxygen2.history.OxygenHistory;
+import ca.ulaval.glo4002.booking.domain.oxygen2.settings.OxygenGradeASettings;
+import ca.ulaval.glo4002.booking.domain.oxygen2.settings.OxygenSupplySettings;
 
 public class OxygenGradeAProducerTest {
+
+    private static final LocalDate ORDER_DATE = LocalDate.now();
+    private static final int SOME_QUANTITY = 26;
+    private static final OxygenSupplySettings SUPPLY_SETTINGS = new OxygenGradeASettings();
+    private static final LocalDate RECEIVED_DATE = ORDER_DATE.plusDays(SUPPLY_SETTINGS.getNumberOfDaysToReceive());
+    private static final int NUMBER_OF_CANDLES_PER_BATCH = 15;
 
     private OxygenGradeAProducer oxygenGradeAProducer;
     private OxygenInventory oxygenInventory;
@@ -25,7 +38,40 @@ public class OxygenGradeAProducerTest {
     }
 
     @Test
-    public void whenOrdering_XXX() {
-        
+    public void whenSupplying_itAddsTheProducedQuantityToTheInventory() {
+        oxygenGradeAProducer.supply(ORDER_DATE, SOME_QUANTITY);
+        int quantityProduced = getQuantityProduced(SOME_QUANTITY, SUPPLY_SETTINGS);
+        verify(oxygenInventory).updateQuantity(SUPPLY_SETTINGS.getGrade(), quantityProduced);
+    }
+
+    @Test
+    public void whenSupplying_itAddsTheProducedQuantityToTheHistoryAtTheReceivedDate() {
+        oxygenGradeAProducer.supply(ORDER_DATE, SOME_QUANTITY);
+        int quantityProduced = getQuantityProduced(SOME_QUANTITY, SUPPLY_SETTINGS);
+        verify(oxygenHistory).addTankMade(RECEIVED_DATE, quantityProduced);
+    }
+
+    @Test
+    public void whenSupplying_itAddsTheUsedCandlesToTheHistoryAtTheOrderDate() {
+        oxygenGradeAProducer.supply(ORDER_DATE, SOME_QUANTITY);
+        int numberOfBatchesProduced = getNumberOfBatchesProduced(SOME_QUANTITY, SUPPLY_SETTINGS);
+        int candlesUsed = numberOfBatchesProduced * NUMBER_OF_CANDLES_PER_BATCH;
+        verify(oxygenHistory).addCandlesUsed(ORDER_DATE, candlesUsed);
+    }
+
+    @Test
+    public void whenSupplying_itAddsTheOutcomeToTheProfitCalculator() {
+        oxygenGradeAProducer.supply(ORDER_DATE, SOME_QUANTITY);
+        int numberOfBatchesProduced = getNumberOfBatchesProduced(SOME_QUANTITY, SUPPLY_SETTINGS);
+        Price cost = SUPPLY_SETTINGS.getCostPerBatch().multipliedBy(numberOfBatchesProduced);
+        verify(profitCalculator).addOutcome(cost);
+    }
+
+    private int getQuantityProduced(int minQuantity, OxygenSupplySettings supplySettings) {
+        return supplySettings.getBatchSize() * getNumberOfBatchesProduced(minQuantity, supplySettings);
+    }
+
+    private int getNumberOfBatchesProduced(int minQuantity, OxygenSupplySettings supplySettings) {
+        return OxygenCalculator.calculateNumberOfBatchesRequired(SOME_QUANTITY, supplySettings.getBatchSize());
     }
 }
