79cdff25c5de58f216276b8038fe6b40bdb50247
vigenere23 <lolgab1@hotmail.com>
2019-11-24 22:24:09 -0500

    added Booking and Application contexts

---
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/context/application/ApplicationContext.java
@@ -0,0 +1,11 @@
+package ca.ulaval.glo4002.booking.context.application;
+
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+
+public class ApplicationContext extends AbstractBinder {
+
+    @Override
+    protected void configure() {
+
+    }
+}
