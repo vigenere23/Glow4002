a6801c887f19cef950e4c0ea5db6a9d026e59d39
vigenere23 <lolgab1@hotmail.com>
2019-11-29 09:06:13 -0500

    readded ShuttleFactory tests

--- booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/transport/ShuttleFactoryTest.java
+++ booking-api/src/test/java/ca/ulaval/glo4002/booking/domain/transport/ShuttleFactoryTest.java
@@ -1,6 +1,6 @@
 package ca.ulaval.glo4002.booking.domain.transport;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
 
@@ -9,30 +9,33 @@ import org.junit.jupiter.api.Test;
 
 class ShuttleFactoryTest {
 
-    // private final static LocalDate DATE = LocalDate.of(2050, 7, 19);
+    private final static LocalDate DATE = LocalDate.now();
+    private final static Direction DIRECTION = Direction.ARRIVAL;
     
-    // private ShuttleFactory shuttleFactory;
+    private ShuttleFactory shuttleFactory;
     
-    // @BeforeEach
-    // public void setUpShuttleFactory() {
-    //     shuttleFactory = new ShuttleFactory();
-    // }
+    @BeforeEach
+    public void setUpShuttleFactory() {
+        shuttleFactory = new ShuttleFactory();
+    }
     
-    // @Test
-    // void givenETSpaceshipCategory_whenCreateShuttle_thenCreatesNewETSpaceship() {
-    //     Shuttle shuttleTest = shuttleFactory.createShuttle(ShuttleCategory.ET_SPACESHIP, DATE);
-    //     assertTrue(shuttleTest instanceof ETSpaceship);
-    // }
+    @Test
+    void givenETSpaceshipCategory_whenCreateShuttle_thenCreatesNewETSpaceship() {
+        Shuttle shuttleTest = shuttleFactory.create(DIRECTION, DATE, ShuttleCategory.ET_SPACESHIP);
+        assertThat(shuttleTest).isInstanceOf(ETSpaceship.class);
+    }
 
-    // @Test
-    // void givenMillenniumFalconCategory_whenCreateShuttle_thenCreatesNewMillenniumFalcon() {
-    //     Shuttle shuttleTest = shuttleFactory.createShuttle(ShuttleCategory.MILLENNIUM_FALCON, DATE);
-    //     assertTrue(shuttleTest instanceof MillenniumFalcon);
-    // }
+    @Test
+    void givenMillenniumFalconCategory_whenCreateShuttle_thenCreatesNewMillenniumFalcon() {
+        Shuttle shuttleTest = shuttleFactory.create(DIRECTION, DATE, ShuttleCategory.MILLENNIUM_FALCON);
+        assertThat(shuttleTest).isInstanceOf(MillenniumFalcon.class);
+
+    }
     
-    // @Test
-    // void givenSpaceXCategory_whenCreateShuttle_thenCreatesNewSpaceX() {
-    //     Shuttle shuttleTest = shuttleFactory.createShuttle(ShuttleCategory.SPACE_X, DATE);
-    //     assertTrue(shuttleTest instanceof SpaceX);
-    // }
+    @Test
+    void givenSpaceXCategory_whenCreateShuttle_thenCreatesNewSpaceX() {
+        Shuttle shuttleTest = shuttleFactory.create(DIRECTION, DATE, ShuttleCategory.SPACE_X);
+        assertThat(shuttleTest).isInstanceOf(SpaceX.class);
+
+    }
 }
