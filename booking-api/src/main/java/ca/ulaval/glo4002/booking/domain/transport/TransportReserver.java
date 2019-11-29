c5f7bc6614f74d0c66d38f874dd22cae98edd35d
vigenere23 <lolgab1@hotmail.com>
2019-11-29 10:17:51 -0500

    added transport reserver tests

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/transport/TransportReserver.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/transport/TransportReserver.java
@@ -9,18 +9,18 @@ public class TransportReserver {
     @Inject private ShuttleFiller shuttleFiller;
     
     public void reserveDeparture(ShuttleCategory shuttleCategory, LocalDate date, PassengerNumber passengerNumber) {
-        reserveDeparture(shuttleCategory, date, passengerNumber, 1);
+        reserveDepartures(shuttleCategory, date, passengerNumber, 1);
     }
 
-    public void reserveDeparture(ShuttleCategory shuttleCategory, LocalDate date, PassengerNumber passengerNumber, int numberOfPassengers) {
+    public void reserveDepartures(ShuttleCategory shuttleCategory, LocalDate date, PassengerNumber passengerNumber, int numberOfPassengers) {
         shuttleFiller.fillShuttles(Direction.DEPARTURE, date, shuttleCategory, passengerNumber, numberOfPassengers);
     }
 
     public void reserveArrival(ShuttleCategory shuttleCategory, LocalDate date, PassengerNumber passengerNumber) {
-        reserveArrival(shuttleCategory, date, passengerNumber, 1);
+        reserveArrivals(shuttleCategory, date, passengerNumber, 1);
     }
 
-    public void reserveArrival(ShuttleCategory shuttleCategory, LocalDate date, PassengerNumber passengerNumber, int numberOfPassengers) {
+    public void reserveArrivals(ShuttleCategory shuttleCategory, LocalDate date, PassengerNumber passengerNumber, int numberOfPassengers) {
         shuttleFiller.fillShuttles(Direction.ARRIVAL, date, shuttleCategory, passengerNumber, numberOfPassengers);
     }   
 }
