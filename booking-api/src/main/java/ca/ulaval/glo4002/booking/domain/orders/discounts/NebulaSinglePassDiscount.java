0924b903b42f1092f90cd87e44d1a80614979064
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2019-12-05 14:55:28 -0500

    cleaned discounts

--- booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/orders/discounts/NebulaSinglePassDiscount.java
+++ booking-api/src/main/java/ca/ulaval/glo4002/booking/domain/orders/discounts/NebulaSinglePassDiscount.java
@@ -1,9 +1,6 @@
 package ca.ulaval.glo4002.booking.domain.orders.discounts;
 
-import java.util.List;
-
 import ca.ulaval.glo4002.booking.domain.Price;
-import ca.ulaval.glo4002.booking.domain.passes.Pass;
 import ca.ulaval.glo4002.booking.domain.passes.PassCategory;
 import ca.ulaval.glo4002.booking.domain.passes.PassOption;
 
@@ -12,18 +9,12 @@ public class NebulaSinglePassDiscount extends OrderDiscount {
     private static final double PERCENTAGE_DISCOUNT = 0.1;
     private static final int NUMBER_OF_ITEMS_REQUIRED = 4;
 
-    @Override
-    public Price getPriceAfterDiscounts(List<Pass> passes, Price totalPrice) {
-        long numberOfSupergiantSinglePass = getQuantityOfMatchingPasses(passes, PassOption.SINGLE_PASS, PassCategory.NEBULA);
-        Price discount = getDiscount(numberOfSupergiantSinglePass, totalPrice);
-        Price priceAfterDiscount = totalPrice.minus(discount);
-
-        return nextDiscount.isPresent()
-            ? nextDiscount.get().getPriceAfterDiscounts(passes, priceAfterDiscount)
-            : priceAfterDiscount;
+    public NebulaSinglePassDiscount() {
+        super(PassOption.SINGLE_PASS, PassCategory.NEBULA);
     }
 
-    private Price getDiscount(long numberOfPasses, Price totalPrice) {
+    @Override
+    protected Price getDiscountAmount(long numberOfPasses, Price totalPrice) {
         return numberOfPasses >= NUMBER_OF_ITEMS_REQUIRED
             ? totalPrice.multipliedBy(PERCENTAGE_DISCOUNT)
             : Price.zero();
